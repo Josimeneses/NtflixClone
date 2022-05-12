3538342ccd07ad1be0be6a827a529946c7e35cc4
Josias Meneses <84454260+Josimeneses@users.noreply.github.com>
2022-05-12 17:46:05 -0300

    Update ROW.js
    increment use state

---
+++ netflix-clone/src/Components/Row.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+function Row ({ title,path }) {
+    const [movies, setMovies]= React.useState([]);
+    
+    const fetchMovies = async (_path) =>{
+       try 
+    }
+
+    return(
+        <div>Row</div>
+    )
+}
+
+export default Row
